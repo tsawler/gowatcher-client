82730bba200582ed5f5759113ca073e6526a7ae8
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-10 12:03:52 -0300

    System load check

--- load.go
+++ load.go
@@ -26,13 +26,11 @@ func checkLoad() (bool, string, string, int, error) {
 
 	msg := fmt.Sprintf("System load okay: %0.4f, %0.4f, %0.4f for %d cpus", systemLoad.Load1, systemLoad.Load5, systemLoad.Load15, numCPU)
 
-	//if systemLoad.Load1 > LoadWarningThreshold * float64(numCPU) {
-	if systemLoad.Load1 > LoadWarningThreshold {
+	if systemLoad.Load1 > LoadWarningThreshold*float64(numCPU) {
 		newStatusID = StatusWarning
 		msg = fmt.Sprintf("Warning: Moderate system load: %0.4f, %0.4f, %0.4f for %d cpus", systemLoad.Load1, systemLoad.Load5, systemLoad.Load15, numCPU)
 		okay = false
-		//} else if systemLoad.Load1 > LoadThreshold * float64(numCPU)  {
-	} else if systemLoad.Load1 > LoadThreshold {
+	} else if systemLoad.Load1 > LoadThreshold*float64(numCPU) {
 		okay = false
 		msg = fmt.Sprintf("Problem: High system load: %0.4f, %0.4f, %0.4f for %d cpus", systemLoad.Load1, systemLoad.Load5, systemLoad.Load15, numCPU)
 		newStatusID = StatusProblem
