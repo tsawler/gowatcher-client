df9f4fd0ff8abcf15e020ea4ce4a06facde6c64b
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-10 12:11:25 -0300

    cleanup

--- load.go
+++ load.go
@@ -10,12 +10,13 @@ const LoadWarningThreshold = 1.5
 const LoadThreshold = 2.0
 
 func checkLoad() (bool, string, string, int, error) {
-
+	// get the system load
 	systemLoad, err := load.Avg()
 	if err != nil {
 		return false, fmt.Sprintf("Error system load: %s", err.Error()), "", StatusProblem, err
 	}
 
+	// count the cpus
 	numCPU, err := cpu.Counts(true)
 	if err != nil {
 		return false, fmt.Sprintf("Error checking CPU: %s", err.Error()), "", StatusProblem, err
@@ -26,6 +27,7 @@ func checkLoad() (bool, string, string, int, error) {
 
 	msg := fmt.Sprintf("System load okay: %0.4f, %0.4f, %0.4f for %d cpus", systemLoad.Load1, systemLoad.Load5, systemLoad.Load15, numCPU)
 
+	// check the threshold * number of cpus to determine if there is a warning/problem
 	if systemLoad.Load1 > LoadWarningThreshold*float64(numCPU) {
 		newStatusID = StatusWarning
 		msg = fmt.Sprintf("Warning: Moderate system load: %0.4f, %0.4f, %0.4f for %d cpus", systemLoad.Load1, systemLoad.Load5, systemLoad.Load15, numCPU)
